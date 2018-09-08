1a61bb0ba7f88fb7a34901a5e1faf52b9cadaced
Zhangji Luo <luozj14@gmail.com>
2018-09-08 14:21:12 -0500

    Initial commit

---
+++ src/main/java/Class1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Class1 {
+
+
+    // Reading data using readLine
+    private String dir;
+
+    public void setDir(){
+        Scanner in = new Scanner(System.in);
+        String host_ = in.nextLine();
+
+        this.dir = host_;
+    }
+    public String getDir() {
+        return dir;
+    }
+
+}
