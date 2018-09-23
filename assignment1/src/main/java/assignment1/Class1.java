83c9be87e8f061a04480453f6eeb4a123e4c2b32
carnivoreYY <yingyi814@gmail.com>
2018-09-23 16:18:33 -0500

    make directory

---
+++ assignment1/src/main/java/assignment1/Class1.java
@@ -0,0 +1,21 @@
+package assignment1;
+
+import java.util.Scanner;
+
+
+public class Class1 {
+
+
+    // Reading data using readLine
+    private String dir;
+
+    public void setDir(String dir){
+
+
+        this.dir = dir;
+    }
+    public String getDir() {
+        return dir;
+    }
+
+}
